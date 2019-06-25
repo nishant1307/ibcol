0ddf2b641c061e55c6b82cf0005043473356d58a
jacky <17038365d@connect.polyu.hk>
2019-06-25 12:00:01 +0800

    updates

--- components/NavLinkComponent/NavLinkComponent.jsx
+++ components/NavLinkComponent/NavLinkComponent.jsx
@@ -24,7 +24,7 @@ class NavLinkComponent extends React.Component {
 
     //console.log('prefetch', prefetch);
 
-    console.log('this.props', this.props);
+    // console.log('this.props', this.props);
 
 
     return (
